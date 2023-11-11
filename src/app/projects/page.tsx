3034225033c9d5b616d5be1034b9eae54698d633
Luis <simosa37@gmail.com>
2023-11-11 15:24:37 -0300

    fix env var

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,14 +4,11 @@ import { getCategories } from "@/utils/getCategories";
 import React from "react";
 import { Project } from "@/lib/projectModel";
 import { getCategoriesCount } from "@/utils/getCategoriesCount";
-import { getDbLogs } from "@/utils/getDbLogs";
-import Logger from "@/components/Logger";
 
 const Projects = async () => {
   const data = await getProjects();
   const categories = await getCategories();
   const categoriesCount = await getCategoriesCount();
-  const dberror = await getDbLogs();
 
   return (
     <section className="w-full bg-white dark:bg-violet-950 border-black p-4 rounded-3xl shadow-md mb-4">
@@ -20,7 +17,6 @@ const Projects = async () => {
         categories={categories}
         categoriesCount={categoriesCount}
       />
-      <Logger message={dberror} />
     </section>
   );
 };
